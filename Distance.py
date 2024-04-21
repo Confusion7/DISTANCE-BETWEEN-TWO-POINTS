40937f7bb23180d7f419c4ab11a50843ca143289
Confusion7 <alaudinsmile4444@gmail.com>
2024-04-21 18:07:03 +0530

    Updated

---
+++ Distance.py
@@ -0,0 +1,13 @@
+#DEVELOPED BY: MOHAMMED SAAJID S
+
+#REGISTER NUMBER: 212223240093
+
+#To calculate the distance between two points
+
+point1 = [4, 2]
+
+point2 = [10, 6]
+
+distance = ((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2) ** 0.5
+
+print(f"{distance:.2f}")
